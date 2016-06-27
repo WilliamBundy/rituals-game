48bd5f3543c67f8a6451515fa6d22710302fb490
William Bundy <william.tc.bundy@gmail.com>
2016-06-27 09:58:45 -0700

    testing stuff...

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,10 @@
 ./find_ssh_agent.sh
+ssh-find-agent -a
+if [ -z "$SSH_AUTH_SOCK" ]
+then
+   eval $(ssh_agent) > /dev/null
+   ssh-add -l >/dev/null || alias ssh='ssh-add -l >/dev/null || ssh-add && unalias ssh; ssh'
+fi
 git add --all
 git commit -m "testing stuff..." 
 git push 
