79a476da08f6f5ef677b1e248cff09e09f38e114
William Bundy <william.tc.bundy@gmail.com>
2016-07-24 14:48:50 -0700

    rituals build 431

--- autogit.sh
+++ autogit.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "rituals build 430" 
+git commit -m "rituals build 431" 
 git push 
--- src/rituals_renderer.cpp
+++ src/rituals_renderer.cpp
@@ -322,7 +322,7 @@ void renderer_push_sprite(Sprite* s)
 		sp.texture.h *= (Renderer->texture_height);
 
 		sp.texture.x += clip.diff.x * (sp.texture.w / sp.size.x);
-		sp.texture.y += clip.diff.y * (sp.texture.h / sp.size.y) ;
+		sp.texture.y += clip.diff.y * (sp.texture.h / sp.size.y);
 		sp.texture.w -= clip.diff.w / clip.r.w;
 		sp.texture.h -= clip.diff.h / clip.r.h;
 
