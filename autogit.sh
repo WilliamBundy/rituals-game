a43c32e0f05c4ace24e11437da3c013c37c9b84e
William Bundy <william.tc.bundy@gmail.com>
2016-07-26 15:33:02 -0700

    rituals build 593

--- autogit.sh
+++ autogit.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "rituals build 592" 
+git commit -m "rituals build 593" 
 git push 
--- src/rituals_gui.cpp
+++ src/rituals_gui.cpp
@@ -489,7 +489,7 @@ void gui_add_slider(Vec2 position, Vec2 size, char* label, real min, real max, i
 		spritefont_render_text(Body_Font,
 				val_str, val_str_len,
 				position, -1, Anchor_Top_Left, 1.0f, &val_region);
-		offset = val_region.x;
+		offset = max_str_len * Body_Font->glyph_width + 4;
 
 	}
 	Vec2 min_str_reg;
