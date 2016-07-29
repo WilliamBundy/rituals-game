ebcc2627a33e39ce617f401e3dc00c8a1a386b97
William Bundy <william.tc.bundy@gmail.com>
2016-07-29 16:26:55 -0700

    rituals build 741

--- autogit.sh
+++ autogit.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "rituals build 740" 
+git commit -m "rituals build 741" 
 git push 
--- src/rituals_world.cpp
+++ src/rituals_world.cpp
@@ -463,6 +463,7 @@ void world_area_update(World_Area* area, World* world)
 	}
 
 	world_area_render(area, world);
+	/*
 	Renderer->offset = v2(0, 0);
 	renderer_start();
 	real lasty = 16;
@@ -471,6 +472,7 @@ void world_area_update(World_Area* area, World* world)
 		lasty += 16;
 	}
 	renderer_draw();
+	*/
 }
 
 
