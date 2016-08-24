bc7576cb20dbcfec54244536466d1f7373308470
William Bundy <william.tc.bundy@gmail.com>
2016-08-24 14:24:17 -0700

    rituals build 950

--- autogit.sh
+++ autogit.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "rituals build 949" 
+git commit -m "rituals build 950" 
 git push 
--- src/rituals_world_area.cpp
+++ src/rituals_world_area.cpp
@@ -250,9 +250,6 @@ void world_area_process_removed_entities(World_Area* area)
 		isize e = area->removed_entities[i];
 		world_area_remove_entity_internal(area, e);
 	}
-	if(area->removed_entities_count != 0) {
-		printf("\n\n");
-	}
 	area->removed_entities_count = 0;
 }
 
