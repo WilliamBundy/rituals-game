8b42c83f8ae02e2e5d0bd2e122ec1ac4e74aa611
William Bundy <william.tc.bundy@gmail.com>
2016-08-25 18:47:28 -0700

    rituals build 1183

--- autogit.sh
+++ autogit.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "rituals build 1182" 
+git commit -m "rituals build 1183" 
 git push 
--- src/wtb_sprite_renderer.h
+++ src/wtb_sprite_renderer.h
@@ -5,10 +5,8 @@
  * Under the MIT and WTFPL licences.
  */ 
 
-#define wtb_pre(n) wtb_##n
 
-
-enum wtb_pre(Sprite_Anchor)
+enum Sprite_Anchor
 {
 	Anchor_Center = 0,
 	Anchor_Top_Left = 1,
@@ -57,5 +55,45 @@ struct Sprite
 	uint32 anchor;
 	Vec2 sort_point_offset;
 };
+
+struct Draw_List
+{
+	Vec2 origin;
+
+	Sprite* sprites;
+	isize sprites_count;
+};
+
+struct Renderer
+{
+	GLuint shader_program, vbo, vao, texture;
+	isize u_screen, u_texturesize, u_window, u_orthomat;
+	
+	Draw_List* draw_lists;
+	isize draw_lists_count;
+};
+
+void render_start(Renderer* r, isize list_index)
+{
+
+}
+
+void render_add(Renderer* r, Sprite* sprite, isize list_index)
+{
+	Draw_List* list = r->draw_lists + list_index;
+	list->sprites[list->sprites_count++] = *sprite;
+}
+
+void render_draw(Renderer* r, isize list_index)
+{
+	Draw_List* list = r->draw_lists + list_index;
+	glBindVertexArray(r->vao);
+	glBindBuffer(GL_ARRAY_BUFFER, r->vbo);
+	glBufferData(GL_ARRAY_BUFFER, 0, NULL, GL_STREAM_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, list->sprites_count * sizeof(Sprite), list->sprites, GL_STREAM_DRAW);
+	glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, list->sprites_count);
+	glBindVertexArray(0);
+}
+
 
 
