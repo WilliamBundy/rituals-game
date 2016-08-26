db560e43060c023ce856197d94fd018088e32001
William Bundy <william.tc.bundy@gmail.com>
2016-08-25 18:51:42 -0700

    rituals build 1186

--- autogit.sh
+++ autogit.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "rituals build 1185" 
+git commit -m "rituals build 1186" 
 git push 
--- src/rituals_main.cpp
+++ src/rituals_main.cpp
@@ -652,6 +652,7 @@ int main(int argc, char** argv)
 				s.position = v2(i % 120, i / 120);
 				s.texture = rect2(3*32/512, 0, 32/512, 32/512);
 				s.size = v2(32, 32);
+				s.color = v4(1, 1, 1, 1);
 				renderer_push_sprite(&s);
 			}
 		}
