c2ac2f60b8f0d5bb131cc2bb70df3af7dda8bc35
William Bundy <william.tc.bundy@gmail.com>
2016-08-25 18:56:40 -0700

    rituals build 1204

--- autogit.sh
+++ autogit.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "rituals build 1203" 
+git commit -m "rituals build 1204" 
 git push 
--- src/rituals_main.cpp
+++ src/rituals_main.cpp
@@ -648,7 +648,7 @@ int main(int argc, char** argv)
 
 		//update();
 		{
-			t += 1;
+			t += .05f;
 			renderer_start();
 			for(isize i = 0; i < 100; ++i) {
 				Sprite s;
