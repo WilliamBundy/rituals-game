646f68fd5b55088796f2c64ae299cf3a3ec4c49c
William Bundy <william.tc.bundy@gmail.com>
2016-09-23 14:59:32 -0700

    rituals build 1387

--- src/rituals_world.cpp
+++ src/rituals_world.cpp
@@ -421,7 +421,7 @@ void init_play_state();
 int recursively_delete_folder(char* path, bool append_base_path);
 void world_area_update(World_Area* area, World* world)
 {
-	game_set_scale(0.25f);
+	game_set_scale(1f);
 	game_calc_mouse_pos(area->offset - Game->size * 0.5f);
 	//Simulation timing
 	
