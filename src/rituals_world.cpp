bd8aff198ec3d9327db9d788aedd98b1af1b60ed
William Bundy <william.tc.bundy@gmail.com>
2016-09-23 15:05:40 -0700

    rituals build 1393

--- src/rituals_world.cpp
+++ src/rituals_world.cpp
@@ -198,7 +198,7 @@ void generate_world_area(World* world, World_Area* area, World_Area_Stub* stub)
 		while (Registry->tiles[tilemap_get_at(&area->map, b->shape.center)].solid);
 	}
 
-	for(isize i = 0; i < WorldAreaTilemapWidth/4 ; ++i) {
+	for(isize i = 0; i < WorldAreaTilemapWidth ; ++i) {
 		Vec2 pos;
 		do {
 			pos = v2(
