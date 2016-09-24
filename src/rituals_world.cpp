df24c7d0b27f66847511f6406c705c5a88cd8bbd
William Bundy <william.tc.bundy@gmail.com>
2016-09-24 15:40:27 -0700

    rituals build 1462

--- src/rituals_world.cpp
+++ src/rituals_world.cpp
@@ -523,8 +523,9 @@ void world_area_update(World_Area* area, World* world)
 					50);
 
 
+			style.time_alpha = false;
 			style.impulse_min = 100;
-			style.impulse_max = 400;
+			style.impulse_max = 200;
 			Vec2 angle_range = v2(a - 0.5f, a + 0.5f);
 			emitter_spawn(&world->emitter, 
 					v3(area->player->sprite.position, 16), 
@@ -535,6 +536,9 @@ void world_area_update(World_Area* area, World* world)
 			style.color = hex_to_v4(0xFFFF33FF);
 			style.time_min = 5;
 			style.time_max = 15;
+			style.impulse_min = 200
+			style.impulse_max = 600;
+			style.acceleration.z = -600;
 			emitter_spawn(&world->emitter, 
 					v3(area->player->sprite.position, 16), 
 					angle_range,
