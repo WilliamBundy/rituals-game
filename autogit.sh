b1512e8f9863863579907f4ff5f1d801b7a8b912
William Bundy <william.tc.bundy@gmail.com>
2016-09-24 15:36:28 -0700

    rituals build 1458

--- autogit.sh
+++ autogit.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "rituals build 1457" 
+git commit -m "rituals build 1458" 
 git push 
--- src/rituals_world.cpp
+++ src/rituals_world.cpp
@@ -544,7 +544,7 @@ void world_area_update(World_Area* area, World* world)
 			style.time_min = 15;
 			style.time_max = 45;
 			style.time_alpha = true;
-			style.time_scale = false;
+			style.time_scaling = false;
 			style.scale_min = 1.5f;
 			style.scale_max = 2.5f; 
 			style.color = hex_to_v4(0xEEEEEE88);
