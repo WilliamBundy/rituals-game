68eb701630abdd4befeb6fdd8fe42546f707047c
William Bundy <william.tc.bundy@gmail.com>
2016-09-25 15:28:24 -0700

    rituals build 1535

--- autogit.sh
+++ autogit.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "rituals build 1534" 
+git commit -m "rituals build 1535" 
 git push 
--- src/rituals_entity_events.cpp
+++ src/rituals_entity_events.cpp
@@ -360,7 +360,7 @@ void rituals_animate_entities(Entity* entities, isize count, World_Area* area, W
 		s.sort_offset += e->z;
 		if(e->anim != NULL) {
 			if(e->anim->current_animation != -1) {
-				Animation* anim = e->anim->animations + e->current_animation;
+				Animation* anim = e->anim->animations + e->anim->current_animation;
 				AnimationFrame* frame = anim->frames + anim->current_frame;
 				s.position += v2(frame->position);
 				s.angle += frame->angle;
