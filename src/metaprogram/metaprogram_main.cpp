c9a0e41a7619e5ee8d0933ee9fea9034fcec1224
William Bundy <william.tc.bundy@gmail.com>
2016-09-29 15:55:15 -0700

    rituals build 31

--- src/metaprogram/metaprogram_main.cpp
+++ src/metaprogram/metaprogram_main.cpp
@@ -298,8 +298,8 @@ int main(int argc, char** argv)
 
 		Struct_Def* s_head = structdef;
 		do {
-			SDL_Assert(0);
 			print_struct(s_head);
+			*((char*)(NULL)) = 'A';
 		} while(s_head = s_head->next);
 		
 
