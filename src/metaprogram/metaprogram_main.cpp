048cf5654a1020bb887bda3f9a8e7b5b43f42bc8
William Bundy <william.tc.bundy@gmail.com>
2016-09-29 12:04:19 -0700

    rituals build 1731

--- src/metaprogram/metaprogram_main.cpp
+++ src/metaprogram/metaprogram_main.cpp
@@ -246,6 +246,17 @@ int main(int argc, char** argv)
 
 		do {
 
+			if(hash_string(p->name, strlen(p->name)) == hash_literal("exclude")) {
+					printf("\n");
+					printf("\n");
+				Token* tt = p->start;
+				do {
+					print_token(tt);
+					printf("\n");
+				} while(tt != p->end);
+					printf("\n");
+					printf("\n");
+			}
 			for(isize i = 0; i < p->decorators_count; ++i) {
 				printf("%s ", p->decorators[i]);
 			}
