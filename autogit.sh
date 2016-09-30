48193bc2bc903503ccf113ce8cfae0eaa0b6b95f
William Bundy <william.tc.bundy@gmail.com>
2016-09-30 14:18:12 -0700

    rituals build 100

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add --all
-git commit -m "rituals build 99" 
+git commit -m "rituals build 100" 
 git push 
 git push github
--- src/metaprogram/metaprogram_main.cpp
+++ src/metaprogram/metaprogram_main.cpp
@@ -294,7 +294,7 @@ int main(int argc, char** argv)
 			printf("typedef struct %s %s;\n", s_head->name, s_head->name);
 		} while(s_head = s_head->next);
 
-		Struct_Def** all_structs = arena_push_array(Work_Arena, Struct_Def, lex.structs_count + 16);
+		Struct_Def** all_structs = arena_push_array(Work_Arena, Struct_Def*, lex.structs_count + 16);
 
 		s_head = structdef;
 		printf("enum Meta_Type\n {\n");
