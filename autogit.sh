0b85cb32f6020aa518ce586909aea5ced35412a2
William Bundy <william.tc.bundy@gmail.com>
2016-09-30 14:41:31 -0700

    rituals build 111

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add --all
-git commit -m "rituals build 110" 
+git commit -m "rituals build 111" 
 git push 
 git push github
--- src/metaprogram/metaprogram_parser.cpp
+++ src/metaprogram/metaprogram_parser.cpp
@@ -950,7 +950,8 @@ void print_struct_names(Struct_Def* def, isize index, char* prefix, isize prefix
 	Struct_Def** local_struct = all_structs + local_counter;
 
 	local_struct[0] = def;
-	def->meta_index = *counter++;
+	def->meta_index = local_counter;
+	*counter = local_counter++;
 	if(index == -1) {
 		chars = printf("\t%.*s_%s,\n", prefix_len, prefix, def->name);
 	} else {
